a51cfb978a95b800728044d1d7e9e897e8dfc6e5
Shreya Yadav <shreyayadav987@gmail.com>
2020-10-03 17:38:47 +0530

    Home page added

--- VirtualStockMarketApp/urls.py
+++ VirtualStockMarketApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('Portfolio/',views.Portfolio)
+    path('Portfolio/',views.portfolio),
+    path('home/',views.home)
 ]
